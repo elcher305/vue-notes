9cf47da74d05bdea914187477e4046c7c4901bcc
Elvira <elvira.chernova.05@gmail.com>
2025-02-09 22:43:03 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -12,4 +12,39 @@ Vue.component('note', {
             <p v-if="card.completedDate">Дата окончания: {{ card.completedDate }}</p>
         </div>
     `,
-})
+    methods: {
+        toggleItem(index) {
+            this.$emit('update-item', { cardIndex: this.card.index, itemIndex: index, columnIndex: this.columnIndex });
+        }
+    }
+});
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            columns: [
+                { title: '3', cards: [], locked: false },
+                { title: '5', cards: [] },
+                { title: 'без ограничений', cards: [] }
+            ],
+            newCardTitle: '',
+            newCardItems: ['', '', '', '', ''],
+            maxCardsInColumnOne: 3,
+            maxCardsInColumnTwo: 5
+        };
+    },
+    created() {
+        const savedData = JSON.parse(localStorage.getItem('noteAppData'));
+        if (savedData) {
+            this.columns = savedData.columns;
+        }
+    },
+    watch: {
+        columns: {
+            deep: true,
+            handler() {
+                localStorage.setItem('noteAppData', JSON.stringify({ columns: this.columns }));
+            }
+        }
+    },
